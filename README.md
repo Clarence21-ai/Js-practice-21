10d1154f3e0db9dd2ef96481b1515693dc946ba4
Clarence21-ai <agtualclarence16@gmail.com>
2024-09-11 02:16:53 +0000

    Initial commit with app.js

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Js-practice-21
+https://github.com/Clarence21-ai/Js-practice-21.git
---
+++ js-practice 21 /app.js
@@ -0,0 +1,3 @@
+console.log('Hello, World!');
+let name = TYPE HERE YOUR NAME;
+console.log('My name is ' + name);
